ccb8bf58c321434ba42c055420f5c6b47ce16c25
Brian Zalewski <bjzalewski@gmail.com>
2026-01-20 21:06:22 -0600

    auto version

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client';
 import router from './router';
 import { RouterProvider } from 'react-router';
 
+const version = process.env.npm_package_version; // Only works if defined in build/env
+// Or more reliably in CRA:
+const appVersion = require('../package.json').version;
+
+document.title = process.env.NODE_ENV === 'development' 
+  ? `DEV - Meme Factory v${appVersion}` 
+  : `Meme Factory v${appVersion}`;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
