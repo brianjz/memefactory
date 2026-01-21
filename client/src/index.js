cf5e107e59c98d3ac9516929f13651592f698270
Brian Zalewski <bjzalewski@gmail.com>
2026-01-20 21:08:28 -0600

    ver

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import router from './router';
 import { RouterProvider } from 'react-router';
 
-const version = process.env.npm_package_version; // Only works if defined in build/env
-// Or more reliably in CRA:
 const appVersion = require('../package.json').version;
 
 document.title = process.env.NODE_ENV === 'development' 
